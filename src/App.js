10844f09f171be8806194e4509d7e13ebeda1865
unknown <sakeerhussain@gmail.com>
2024-08-09 14:58:44 +0530

    new commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import "./App.css";
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
+import Todo from "./components/Todo";
+import { nanoid } from "nanoid";
+import { useState } from "react";
+
+function App(props) {
+  console.log(props.tasks); //data in index.js, <App tasks={DATA}
+
+  const taskList = props.tasks?.map((task) => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+    />
+  ));
+
+  return (
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+
+      <Form />
+      <div className="filters btn-group stack-exception">
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
+      </div>
+      <h2 id="list-heading">3 tasks remaining</h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {taskList}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
