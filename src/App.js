b9b8233a500fe701f7811d113ee707593a071bd3
unknown <sakeerhussain@gmail.com>
2024-08-10 13:16:55 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,54 @@
-import "./App.css";
+import { useState } from "react";
+import { nanoid } from "nanoid";
+import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
-import Todo from "./components/Todo";
-import { nanoid } from "nanoid";
-import { useState } from "react";
 
 function App(props) {
-
-  const[tasks, setTasks] = useState(props.tasks);
-  console.log(props.tasks); //data in index.js, <App tasks={DATA}
-
   //callback props- this function is expecting an input 'name' from the Form
-  function addTask(name){
-    const newTask = { id: `todo-${nanoid()}`, name, completed: false};
+  function addTask(name) {
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false };
     setTasks([...tasks, newTask]);
   }
+
+  // toggleTaskCompleted is used to update the completed status(callback props)
   function toggleTaskCompleted(id) {
-   const updatedTasks = tasks.map((task) => {
-    // if this task has the same ID as the edited task
-    if(id === task.id) {
-      //use object spread to make a new object
-      // whose 'completed' prps has been inverted
-      return { ...task, completed: !task.completed };
-    }
+    const updatedTasks = tasks.map((task) => {
+      //if this task has the same id as the edited task
+      if (id === task.id) {
+        //use object spread to make a new object
+        // whose 'completed' props has been inverted
+        return { ...task, completed: !task.completed };
+      }
       return task;
-   });
-   setTasks(updatedTasks);
+    });
+    setTasks(updatedTasks);
   }
+
+  // The deleteTask callback props
+  function deleteTask(id) {
+    console.log(id);
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+  // The EditTask callback props
+  function editTask(id, newName) {
+    console.log(id);
+    const editedTaskList = tasks.map((task) => {
+      //if this task has the same ID as the edited task
+      if (id === task.id) {
+        // copy the task and update its name
+        return { ...task, name: newName };
+      }
+      //Return the original task if it's not edited task
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+
+  const [tasks, setTasks] = useState(props.tasks);
+  /*console.log(props.tasks); data in index.js, <App tasks={DATA}*/
   const taskList = tasks?.map((task) => (
     <Todo
       id={task.id}
@@ -34,9 +56,11 @@ function App(props) {
       completed={task.completed}
       key={task.id}
       toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+      editTask={editTask}
     />
   ));
-  //For counting the no: tasks 
+  //For counting the no: tasks
   const taskNoun = taskList.length !== 1 ? "tasks" : "task";
   const headingText = `${taskList.length} ${taskNoun} remaining`;
 
@@ -63,4 +87,3 @@ function App(props) {
 }
 
 export default App;
-
